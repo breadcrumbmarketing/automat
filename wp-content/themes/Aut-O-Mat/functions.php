4653244ccef8c6b27c3e8f2de3416f51ad447824
hamyvosugh <online@breadcrumb.de>
2025-02-07 16:02:18 +0100

    asd

--- wp-content/themes/Aut-O-Mat/functions.php
+++ wp-content/themes/Aut-O-Mat/functions.php
@@ -71,3 +71,13 @@ function enqueue_general_assets() {
     }
 }
 add_action('wp_enqueue_scripts', 'enqueue_general_assets');
+
+// general pages
+function enqueue_fragen_assets() {
+    if (is_page_template('fragen.php')) {
+        // Use get_stylesheet_directory_uri() for child themes
+        wp_enqueue_style('general-css', get_stylesheet_directory_uri() . '/assets/css/fragen.css', array(), '1.0.0');
+        wp_enqueue_script('general-js', get_stylesheet_directory_uri() . '/assets/js/fragen.js', array('jquery'), '1.0.0', true);
+    }
+}
+add_action('wp_enqueue_scripts', 'enqueue_gefragen_assets');
--- wp-content/themes/Aut-O-Mat/general.php
+++ wp-content/themes/Aut-O-Mat/general.php
@@ -1,21 +1,30 @@
-<?php
-/**
- * Template Name: General
- * Description: A blank template for Elementor content.
- * 
- * @package Aut-O-Mat
- */
-// Ensure Elementor compatibility
-get_header();
-?>
 
+/* Button Hover */
+.elementor-button {
+    position: relative !important;
+    overflow: hidden !important;
+    transition: color 0.3s ease !important;
+    z-index: 1 !important;
+}
 
-    <?php 
-    // Display Elementor content if available
-    while ( have_posts() ) : the_post();
-        the_content(); // This allows Elementor to load its content
-    endwhile;
-    ?>
-    
+.elementor-button:before {
+    content: '' !important;
+    position: absolute !important;
+    top: 0 !important;
+    left: -170% !important;
+    width: 150% !important;
+    height: 100% !important;
+    background: #000 !important;
+    transform: skewX(-45deg) !important;
+    transition: all 0.5s ease !important;
+    z-index: -1 !important;
+}
 
-<?php get_footer(); ?>
+.elementor-button:hover {
+    color: #fff !important;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2), 0 6px 12px rgba(0, 0, 0, 0.1), 0 8px 16px rgba(0, 0, 0, 0.1) !important;
+}
+
+.elementor-button:hover:before {
+    left: -25% !important;
+}
